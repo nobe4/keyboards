e6147afc5fb506257f5ee42b21233b0863868f23
nobe4 <nobe4@users.noreply.github.com>
2023-06-10 14:03:58 +0200

    Add colemak dh, simplify other options

--- nobe4/config.h
+++ nobe4/config.h
@@ -3,28 +3,3 @@
 #define NO_ACTION_TAPPING
 #define NO_ACTION_ONESHOT
 #define NO_ACTION_MACRO
-
-/*
- * MIDI options
- */
-
-/* Prevent use of disabled MIDI features in the keymap */
-// #define MIDI_ENABLE_STRICT 1
-// #define MIDI_ENABLE 1
-
-/* enable basic MIDI features:
-   - MIDI notes can be sent when in Music mode is on
-*/
-
-// #define MIDI_BASIC 1
-
-/* enable advanced MIDI features:
-   - MIDI notes can be added to the keymap
-   - Octave shift and transpose
-   - Virtual sustain, portamento, and modulation wheel
-   - etc.
-*/
-// #define MIDI_ADVANCED 1
-
-/* override number of MIDI tone keycodes (each octave adds 12 keycodes and allocates 12 bytes) */
-// #define MIDI_TONE_KEYCODE_OCTAVES 2
