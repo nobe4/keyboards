5bb5fa49a33c170a41d612a3100d8c841cf929d2
nobe4 <nobe4@users.noreply.github.com>
2023-06-16 20:27:08 +0200

    Move to colemak

--- nobe4/keymap.c
+++ nobe4/keymap.c
@@ -1,7 +1,7 @@
 #include QMK_KEYBOARD_H
 
 enum layer_names {
-  _CL, // Colemak DH
+  _CL, // Colemak
   _QW, // Qwerty
   _SY, // symbols
   _OS, // Operating System
@@ -9,11 +9,10 @@ enum layer_names {
 
 // clang-format off
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-	// keep QWERTY for gaming
 	[_CL] = LAYOUT_planck_grid(
-		KC_TAB,  KC_Q,    KC_W,    KC_F,    KC_P,    KC_B,    KC_J,    KC_L,    KC_U,    KC_Y,    KC_SCLN, KC_BSPC,
-		KC_ESC,  KC_A,    KC_R,    KC_S,    KC_T,    KC_G,    KC_M,    KC_N,    KC_E,    KC_I,    KC_O,    KC_QUOT,
-		KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_D,    KC_V,    KC_K,    KC_H,    KC_COMM, KC_DOT,  KC_SLSH, KC_ENT,
+		KC_TAB,  KC_Q,    KC_W,    KC_F,    KC_P,    KC_G,    KC_J,    KC_L,    KC_U,    KC_Y,    KC_SCLN, KC_BSPC,
+		KC_ESC,  KC_A,    KC_R,    KC_S,    KC_T,    KC_D,    KC_H,    KC_N,    KC_E,    KC_I,    KC_O,    KC_QUOT,
+		KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_K,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_ENT,
 		KC_LCTL, KC_LGUI, KC_LALT, KC_LGUI, MO(_SY), KC_SPC,  KC_SPC,  MO(_OS), KC_RGUI, KC_RALT, KC_RCTL, DF(_QW)
 	),
 	// keep QWERTY for gaming
