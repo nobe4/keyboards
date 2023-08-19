2ed1897122213e20547143e87e08c07dc1b1ad0f
nobe4 <nobe4@users.noreply.github.com>
2023-08-19 12:57:27 +0200

    Add more changes

--- nobe4/keymap.c
+++ nobe4/keymap.c
@@ -13,6 +13,7 @@ enum layouts {
 enum tap_dance {
   START, // keep first
 
+  Q_HASH,
   P_PRC,
   QO_GR,
 
@@ -34,43 +35,44 @@ enum tap_dance {
 
 // clang-format off
 tap_dance_action_t tap_dance_actions[] = {
-  TD_DEF(P_PRC, KC_P,    KC_PERC)
-  TD_DEF(QO_GR, KC_QUOT, KC_GRV)
+  TD_DEF(Q_HASH, KC_Q,    KC_HASH)
+  TD_DEF(P_PRC,  KC_P,    KC_PERC)
+  TD_DEF(QO_GR,  KC_QUOT, KC_GRV)
 
-  TD_DEF(A_AT,  KC_A,    KC_AT)
-  TD_DEF(S_DLR, KC_S,    KC_DLR)
-  TD_DEF(R_AMP, KC_R,    KC_AMPR)
-  TD_DEF(O_SCL, KC_O,    KC_SCLN)
+  TD_DEF(A_AT,   KC_A,    KC_AT)
+  TD_DEF(S_DLR,  KC_S,    KC_DLR)
+  TD_DEF(R_AMP,  KC_R,    KC_AMPR)
+  TD_DEF(O_SCL,  KC_O,    KC_SCLN)
 
-  TD_DEF(CO_UD, KC_COMM, KC_UNDS)
-  TD_DEF(DO_EX, KC_DOT,  KC_EXLM)
-  TD_DEF(SL_BL, KC_SLSH, KC_BSLS)
+  TD_DEF(CO_UD,  KC_COMM, KC_UNDS)
+  TD_DEF(DO_EX,  KC_DOT,  KC_EXLM)
+  TD_DEF(SL_BL,  KC_SLSH, KC_BSLS)
 };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [LY_CL] = LAYOUT_planck_grid(
-    KC_Q,     KC_W,      KC_F,      TD(P_PRC),  KC_G,        ___, /**/ ___, KC_J,         KC_L,      KC_U,      KC_Y,      TD(QO_GR),
-    TD(A_AT), TD(R_AMP), TD(S_DLR), _MG(KC_T),  KC_D,        ___, /**/ ___, KC_H,         KC_N,      KC_E,      KC_I,      TD(O_SCL),
-    KC_Z,     KC_X,      KC_C,      KC_V,       _MG(KC_B),   ___, /**/ ___, KC_K,         KC_M,      TD(CO_UD), TD(DO_EX), TD(SL_BL),
-    ___,      ___,       ___,       OSL(LY_SY), _MS(KC_SPC), ___, /**/ ___, _MC(KC_BSPC), KC_ENT,    ___,       ___,       ___
+    TD(Q_HASH), KC_W,      KC_F,      TD(P_PRC), KC_G,        ___, /**/ ___, KC_J,         KC_L,       KC_U,      KC_Y,      TD(QO_GR),
+    TD(A_AT),   TD(R_AMP), TD(S_DLR), KC_T,      KC_D,        ___, /**/ ___, KC_H,         KC_N,       KC_E,      KC_I,      TD(O_SCL),
+    KC_Z,       KC_X,      KC_C,      KC_V,      _MG(KC_B),   ___, /**/ ___, KC_K,         KC_M,       TD(CO_UD), TD(DO_EX), TD(SL_BL),
+    ___,        ___,       ___,       KC_ENT,    _MS(KC_SPC), ___, /**/ ___, _MC(KC_BSPC), OSL(LY_SY), ___,       ___,       ___
   ),
   [LY_SY] = LAYOUT_planck_grid(
-    KC_1,     KC_2,      KC_3,      KC_4,       KC_5,        ___, /**/ ___, KC_6,         KC_7,      KC_8,      KC_9,      KC_0,
-    KC_ESC,   ___,       KC_ASTR,   ___,        ___,         ___, /**/ ___, KC_MINS,      KC_PLUS,   KC_EQL,    KC_LPRN,   KC_RPRN,
-    KC_TAB,   ___,       ___,       KC_HASH,    ___,         ___, /**/ ___, KC_CIRC,      KC_LCBR,   KC_RCBR,   KC_LBRC,   KC_RBRC,
-    ___,      ___,       ___,       TO(LY_OS),  KC_LSFT,     ___, /**/ ___, KC_DEL,       TO(LY_CL), ___,       ___,       ___
+    KC_1,       KC_2,      KC_3,      KC_4,      KC_5,        ___, /**/ ___, KC_6,         KC_7,       KC_8,      KC_9,      KC_0,
+    KC_ESC,     ___,       KC_ASTR,   ___,       ___,         ___, /**/ ___, KC_MINS,      KC_PLUS,    KC_EQL,    KC_LPRN,   KC_RPRN,
+    KC_TAB,     ___,       ___,       ___,       ___,         ___, /**/ ___, KC_CIRC,      KC_LCBR,    KC_RCBR,   KC_LBRC,   KC_RBRC,
+    ___,        ___,       ___,       TO(LY_CL), KC_LSFT,     ___, /**/ ___, KC_DEL,       TO(LY_OS),  ___,       ___,       ___
   ),
   [LY_OS] = LAYOUT_planck_grid(
-    ___,      ___,       ___,       ___,        ___,         ___, /**/ ___, ___,          ___,       ___,       ___,       ___,
-    ___,      KC_MUTE,   KC_VOLD,   KC_VOLU,    ___,         ___, /**/ ___, KC_LEFT,      KC_DOWN,   KC_UP,     KC_RGHT,   ___,
-    ___,      KC_MRWD,   KC_MPLY,   KC_MFFD,    ___,         ___, /**/ ___, KC_HOME,      KC_PGDN,   KC_PGUP,   KC_END,    ___,
-    ___,      ___,       ___,       TO(LY_FN),  ___,         ___, /**/ ___, KC_LCTL,      TO(LY_CL), ___,       ___,       ___
+    ___,        ___,       ___,       ___,       ___,         ___, /**/ ___, ___,          ___,        ___,       ___,       ___,
+    ___,        KC_MUTE,   KC_VOLD,   KC_VOLU,   ___,         ___, /**/ ___, KC_LEFT,      KC_DOWN,    KC_UP,     KC_RGHT,   ___,
+    ___,        KC_MRWD,   KC_MPLY,   KC_MFFD,   ___,         ___, /**/ ___, ___,          ___,        ___,       ___,       ___,
+    ___,        ___,       ___,       TO(LY_CL), ___,         ___, /**/ ___, KC_LCTL,      TO(LY_FN),  ___,       ___,       ___
   ),
   [LY_FN] = LAYOUT_planck_grid(
-    KC_F1,    KC_F2,     KC_F3,     KC_F4,      KC_F5,       ___, /**/ ___, ___,          ___,       ___,       ___,       ___,
-    KC_F6,    KC_F7,     KC_F8,     KC_F9,      KC_F10,      ___, /**/ ___, ___,          ___,       ___,       ___,       ___,
-    KC_F11,   KC_F12,    ___,       ___,        ___,         ___, /**/ ___, ___,          ___,       ___,       ___,       ___,
-    ___,      ___,       ___,       ___,        ___,         ___, /**/ ___, ___,          TO(LY_CL), ___,       ___,       ___
+    KC_F1,      KC_F2,     KC_F3,     KC_F4,     KC_F5,       ___, /**/ ___, ___,          ___,        ___,       ___,       ___,
+    KC_F6,      KC_F7,     KC_F8,     KC_F9,     KC_F10,      ___, /**/ ___, ___,          ___,        ___,       ___,       ___,
+    KC_F11,     KC_F12,    ___,       ___,       ___,         ___, /**/ ___, ___,          ___,        ___,       ___,       ___,
+    ___,        ___,       ___,       TO(LY_CL), ___,         ___, /**/ ___, ___,          ___,        ___,       ___,       ___
   ),
 };
 // clang-format on
