350845cfe4a640db378eb6f0a7c38606e972e943
nobe4 <nobe4@users.noreply.github.com>
2025-07-10 18:05:31 +0200

    chore(devenv): simplify

--- devenv.nix
+++ devenv.nix
@@ -11,28 +11,17 @@
     clean.exec = "rm -rf ./qmk";
 
     install.exec = ''
-      ${pkgs.qmk}/bin/qmk config user.qmk_home=$(pwd)/qmk
-      ${pkgs.qmk}/bin/qmk setup -y
+      qmk config user.qmk_home=$(pwd)/qmk
+      qmk setup -y
 
       ln -s $(pwd)/planck $(pwd)/qmk/keyboards/planck/keymaps/nobe4
       ln -s $(pwd)/ferris $(pwd)/qmk/keyboards/ferris/keymaps/nobe4
     '';
 
-    watch.exec = ''
-      ${pkgs.git}/bin/git ls-files | \
-          entr -c -r $1
-    '';
+    rev5.exec = "qmk flash -kb planck/rev5 -km nobe4";
+    rev6.exec = "qmk flash -kb planck/rev6 -km nobe4";
+    ferris.exec = "CONVERT_TO=rp2040_ce qmk flash -kb ferris/sweep -km nobe4";
 
-    rev5.exec = ''
-      ${pkgs.qmk}/bin/qmk flash -kb planck/rev5 -km nobe4
-    '';
-
-    rev6.exec = ''
-      ${pkgs.qmk}/bin/qmk flash -kb planck/rev6 -km nobe4
-    '';
-
-    ferris.exec = ''
-      CONVERT_TO=rp2040_ce ${pkgs.qmk}/bin/qmk flash -kb ferris/sweep -km nobe4
-    '';
+    watch.exec = "git ls-files | entr -c -r $1";
   };
 }
